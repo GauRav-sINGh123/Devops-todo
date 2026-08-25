3ffa1df3231330ea30db8abae93bd1aa7cf01883
Gaurav Singh <gksingh947@gmail.com>
2026-08-25 19:15:51 +0530

    Env path fix and port fix

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,7 @@ import dotenv from 'dotenv';
 import {connectDB} from './db/db.js';
 import {app} from './app.js';
 
-dotenv.config(
-    {
-        path:'./.env'
-    }
-);
+dotenv.config();
 
 connectDB().then(()=>{
     app.listen(process.env.PORT || 5000,()=>{
